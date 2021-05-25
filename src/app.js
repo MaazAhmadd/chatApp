f203b045ee529f12599b05607dd3f6f46a1f0970
Maaz <maaz.ahmad21399@gmail.com>
2021-05-25 17:14:29 +0500

    added https

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
-let express = require( 'express' );
+let express = require("express");
 let app = express();
-let server = require( 'http' ).Server( app );
-let io = require( 'socket.io' )( server );
-let stream = require( './ws/stream' );
-let path = require( 'path' );
-let favicon = require( 'serve-favicon' );
+const fs = require("fs");
+let port = process.env.PORT || 3000;
+const httpsServer = require("https")
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert"),
+    },
+    app
+  )
+  .listen(port, function () {
+    console.log(`Listening on port ${port}...`);
+  });
+// let server = require("http").Server(app);
+let io = require("socket.io")(httpsServer);
+let stream = require("./ws/stream");
+let path = require("path");
+let favicon = require("serve-favicon");
 
-app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
-app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
+app.use(favicon(path.join(__dirname, "favicon.ico")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
-app.get( '/', ( req, res ) => {
-    res.sendFile( __dirname + '/index.html' );
-} );
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
+io.of("/stream").on("connection", stream);
 
-io.of( '/stream' ).on( 'connection', stream );
-
-server.listen( 3000 );
+// server.listen(3000);
