834bdbb29dd216b172406d6157f2e9d1e95ca530
Maaz <maaz.ahmad21399@gmail.com>
2021-06-04 08:39:59 +0500

    fixed

--- src/app.js
+++ src/app.js
@@ -1,47 +1,22 @@
 let express = require("express");
 let app = express();
-// const fs = require("fs");
-var cors = require("cors");
-let maxusers = require("./maxusers");
-let port = process.env.PORT || 3000;
-// const httpsServer = require("https")
-//   .createServer(
-//     {
-//       cert: fs.readFileSync("cert.crt"),
-//       ca: fs.readFileSync("ca.crt.ca-bundle"),
-//       key: fs.readFileSync("private.key"),
-//       // cert: fs.readFileSync("server.cert"),
-//       // key: fs.readFileSync("server.key"),
-//     },
-//     app
-//   )
-// .listen(port, function () {
-//   console.log(`Listening on port ${port}...`);
-// });
 let server = require("http").Server(app);
-let io = require("socket.io")(
-  server
-  // { reconnect: false }
-);
+let io = require("socket.io")(server);
+// { reconnect: false }
 // let stream = require("./ws/stream");
 let path = require("path");
 let favicon = require("serve-favicon");
+let port = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(favicon(path.join(__dirname, "favicon.ico")));
 app.use("/assets", express.static(path.join(__dirname, "assets")));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// app.post("/sendmaxuser", (req, res) => {
-//   maxusers.setmaxusers(0);
-//   maxusers.setmaxusers(req.body.maxusers);
-//   res.status(200).send("everything ok");
-// });
 let len = 0;
 io.of("/stream").on("connection", (socket) => {
   socket.on("subscribe", (data) => {
